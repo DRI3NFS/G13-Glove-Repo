d68ecaba8904c8a294d040b2b8cf726fcc8c10bf
DRI3NFS <lejanorr@myumanitoba.ca>
2025-03-13 15:02:49 -0500

    Latest Test Code

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 #include <std_msgs/msg/string.h>
 #include <std_msgs/msg/u_int8.h>
 
-//#define COMMTEST //Disable commtest if only testing glove stuff
+#define COMMTEST //Disable commtest if only testing glove stuff
 
 // Code for MPU6050 sensor was referenced from the following repository: https://github.com/ElectronicCats/mpu6050/tree/master/examples
 // NOTE: accelerometer data values end up as estimates, if we were to leave the MPU6050 in a fixed position, we expect the outputs to be 0,0 and 9.8ms2 but it is not.
@@ -46,11 +46,8 @@ FlexSensor rngFing(35);
 FlexSensor pnkFing(32);
 tiltSensor handTilt(&handMPU);
 
-bool Calibrate = false;
-
 uint8_t dataPackage;       //THIS IS THE DATA PACKAGE THAT WILL BE SENT TO THE COMPUTER
 
-#ifdef COMMTEST
 /* Hotspot */
 char* ssid = "edcel";
 char* pass = "edcel1234";
@@ -72,26 +69,24 @@ std_msgs__msg__UInt8 msg;
 rcl_publisher_t publisher;
 
 //  Timer callback function
-void publish_callback(rcl_timer_t* timer, int64_t last_call_time)
-{
-    if (timer == NULL) return;
+// void publish_callback(rcl_timer_t* timer, int64_t last_call_time)
+// {
+//     if (timer == NULL) return;
 
-    //  Fill the message
-    msg.data = dataPackage;
+//     //  Fill the message
+//     msg.data = dataPackage;
 
-    //  Publish the message 
-    if (rcl_publish(&publisher, &msg, NULL) == RCL_RET_OK)
-    {
-      Serial.println("Published!");
-    }
-    else 
-    {
-      Serial.println("Failed to Publish! :(");
-    }
+//     //  Publish the message 
+//     if (rcl_publish(&publisher, &msg, NULL) == RCL_RET_OK)
+//     {
+//       Serial.println("Published!");
+//     }
+//     else 
+//     {
+//       Serial.println("Failed to Publish! :(");
+//     }
     
-}
-
-#endif
+// }
 
 void setup(void) {
   Serial.begin(115200);
@@ -105,98 +100,98 @@ void setup(void) {
     Serial.println("MPU Test for Hand Successful");
   }
 
-  #ifdef COMMTEST
     //  Initialize micro-ROS transport
-    set_microros_wifi_transports(ssid, pass, agent_ip, atoi(agent_port));
+    // set_microros_wifi_transports(ssid, pass, agent_ip, atoi(agent_port));
 
-    //  Ping the microROS Agent 
-    Serial.print("Pinging micro-ROS Agent... ");
-    if (rmw_uros_ping_agent(2000, 10)) 
-    {  // Wait up to 2 seconds, try 10 times
-        Serial.println("Connected to micro-ROS Agent");
-    } else 
-    {
-        Serial.println("Failed to connect to micro-ROS Agent");
-    }
+    // Serial.println("Wifi Setup");
 
-    //  Initialize micro-ROS
-    allocator = rcl_get_default_allocator();
+    // //  Ping the microROS Agent 
+    // Serial.print("Pinging micro-ROS Agent... ");
+    // if (rmw_uros_ping_agent(2000, 10)) 
+    // {  // Wait up to 2 seconds, try 10 times
+    //     Serial.println("Connected to micro-ROS Agent");
+    // } else 
+    // {
+    //     Serial.println("Failed to connect to micro-ROS Agent");
+    // }
 
-    //  Initialize RCL init options 
-    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
-    rcl_ret_t ret = rcl_init_options_init(&init_options, allocator);
-    if (ret != RCL_RET_OK)
-    {
-        Serial.println("Failed to initialize RCL init options");
-        return;
-    }
-    else
-    {
-        Serial.println("RCL init options initialized.");  
-    }
+    // //  Initialize micro-ROS
+    // allocator = rcl_get_default_allocator();
 
-    //  Set ROS DOMAIN ID Option to 
-    size_t domain_id = 42;
-    ret = rcl_init_options_set_domain_id(&init_options, domain_id);
-    if (ret != RCL_RET_OK)
-    {
-        Serial.println("\nFailed to set domain ID");
-        return;
-    }
-    else
-    {
-        Serial.println("ROS_DOMAIN_ID Set.");  
-    }
+    // //  Initialize RCL init options 
+    // rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    // rcl_ret_t ret = rcl_init_options_init(&init_options, allocator);
+    // if (ret != RCL_RET_OK)
+    // {
+    //     Serial.println("Failed to initialize RCL init options");
+    //     return;
+    // }
+    // else
+    // {
+    //     Serial.println("RCL init options initialized.");  
+    // }
+
+    // //  Set ROS DOMAIN ID Option to 
+    // size_t domain_id = 42;
+    // ret = rcl_init_options_set_domain_id(&init_options, domain_id);
+    // if (ret != RCL_RET_OK)
+    // {
+    //     Serial.println("\nFailed to set domain ID");
+    //     return;
+    // }
+    // else
+    // {
+    //     Serial.println("ROS_DOMAIN_ID Set.");  
+    // }
     
-    // Initialize micro-ROS support with default options
-    ret = rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator);
-    if (ret != RCL_RET_OK) 
-    {
-        Serial.printf("\nFailed to initialize micro-ROS support, error %d\n", ret);
-        return;
-    }
-    else
-    {
-        Serial.println("micro-ROS support initialized.");  
-    }
+    // // Initialize micro-ROS support with default options
+    // ret = rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator);
+    // if (ret != RCL_RET_OK) 
+    // {
+    //     Serial.printf("\nFailed to initialize micro-ROS support, error %d\n", ret);
+    //     return;
+    // }
+    // else
+    // {
+    //     Serial.println("micro-ROS support initialized.");  
+    // }
 
-    //  Node Creation
-    ret = rclc_node_init_default(&node, "esp32_publisher_node", "", &support);
-    if (ret != RCL_RET_OK)
-    {
-        Serial.printf("\nFailed to initialize node, error: %d\n", ret);  
-        return;
-    }
-    else 
-    {
-        Serial.println("Node Created.");
-    }
+    // //  Node Creation
+    // ret = rclc_node_init_default(&node, "esp32_publisher_node", "", &support);
+    // if (ret != RCL_RET_OK)
+    // {
+    //     Serial.printf("\nFailed to initialize node, error: %d\n", ret);  
+    //     return;
+    // }
+    // else 
+    // {
+    //     Serial.println("Node Created.");
+    // }
 
-    //  Create a publisher 
-    rclc_publisher_init_default(
-      &publisher,
-      &node,
-      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt8),
-      "/esp32_glove"
-    );
+    // //  Create a publisher 
+    // rclc_publisher_init_default(
+    //   &publisher,
+    //   &node,
+    //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt8),
+    //   "/esp32_glove"
+    // );
 
-    //  Create a timer (1 second period)
-    rclc_timer_init_default(
-      &timer,
-      &support,
-      RCL_MS_TO_NS(1000),
-      publish_callback
-    );
+    // //  Create a timer (1 second period)
+    // rclc_timer_init_default(
+    //   &timer,
+    //   &support,
+    //   RCL_MS_TO_NS(1000),
+    //   publish_callback
+    // );
 
-    //  Initialize executor
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
-    rclc_executor_add_timer(&executor, &timer);
+    // //  Initialize executor
+    // rclc_executor_init(&executor, &support.context, 1, &allocator);
+    // rclc_executor_add_timer(&executor, &timer);
 
-    //  Message Initialization
-    msg.data = NULL;
+    // //  Message Initialization
+    // msg.data = NULL;
     
-    Serial.println("microROS setup complete!");
-    #endif
+    // Serial.println("microROS setup complete!");
 }
 
 void loop() {
@@ -224,11 +219,14 @@ void loop() {
   dataPackage = dataPackage + (pnkFing.flexCheck() << 4);
   dataPackage = dataPackage + (tmbFing.flexCheck() << 3);
   dataPackage = dataPackage + handTilt.getOrientation();
-#ifdef COMMTEST
-  Serial.println("Spinning...");
-  //  Spin Executor
-  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-#endif
+
+  Serial.printf("ID is: %x", dataPackage);
+  Serial.println("");
+
+  // Serial.println("Spinning...");
+  // //  Spin Executor
+  // rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+
   //  Delay to control publishing rate
-  delay(10); 
+  delay(100); 
 }
